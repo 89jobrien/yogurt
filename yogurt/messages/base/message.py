8ef8cd05e2a0e03afd04ab8cb22dc25aa96b7a36
jprobrien <98370624+89jobrien@users.noreply.github.com>
2025-08-03 11:10:39 -0400

    Update message.py

--- yogurt/messages/base/message.py
+++ yogurt/messages/base/message.py
@@ -1,22 +1,30 @@
 from typing import Literal
 from pydantic import BaseModel
 
+
 class BaseMessage(BaseModel):
     """The base class for a message."""
+
     content: str
-    role: str # 'human', 'ai', 'system'
+    role: str  # 'human', 'ai', 'system'
 
     def __str__(self):
         return self.content
 
+
 class HumanMessage(BaseMessage):
     """A message from a human."""
-    role: Literal['human'] = 'human'
+
+    role: Literal["human"] = "human"
+
 
 class AIMessage(BaseMessage):
     """A message from the AI."""
-    role: Literal['ai'] = 'ai'
+
+    role: Literal["ai"] = "ai"
+
 
 class SystemMessage(BaseMessage):
     """A system message that sets the context for the AI."""
-    role: Literal['system'] = 'system'
+
+    role: Literal["system"] = "system"
