61e4052c7d9453e67c8bd1cb5bc0f296013a4403
jprobrien <98370624+89jobrien@users.noreply.github.com>
2025-08-03 11:10:02 -0400

    Update document_loader.py

--- yogurt/document_loaders/base/document_loader.py
+++ yogurt/document_loaders/base/document_loader.py
@@ -2,8 +2,10 @@ from abc import ABC, abstractmethod
 from typing import List
 from yogurt.documents import Document
 
+
 class BaseDocumentLoader(ABC):
     """Interface for loading Documents from a source."""
+
     @abstractmethod
     def load(self) -> List[Document]:
         pass
