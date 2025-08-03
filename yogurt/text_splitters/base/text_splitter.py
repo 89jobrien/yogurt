700bd20be00634bb816700e427ec3752a7b8ee42
jprobrien <98370624+89jobrien@users.noreply.github.com>
2025-08-03 11:11:48 -0400

    Update text_splitter.py

--- yogurt/text_splitters/base/text_splitter.py
+++ yogurt/text_splitters/base/text_splitter.py
@@ -2,8 +2,10 @@ from abc import ABC, abstractmethod
 from typing import List
 from yogurt.documents.base import Document
 
+
 class BaseTextSplitter(ABC):
     """Interface for splitting text into smaller chunks."""
+
     @abstractmethod
     def split_documents(self, documents: List[Document]) -> List[Document]:
         pass
